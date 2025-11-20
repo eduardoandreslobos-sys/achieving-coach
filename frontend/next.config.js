ac6b726594f0f03c66b8464f0ce73c6dc2cfc71f
Eduardo Lobos <eduardoandres.lobos@gmail.com>
2025-11-20 18:48:43 +0000

    Use dummy env vars for build, real vars at runtime

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: {
-    // Disable static page generation for pages that use Firebase
-    outputFileTracingIncludes: {
-      '/': ['./node_modules/**/*.wasm', './node_modules/**/*.node'],
-    },
-  },
-  // Don't try to prerender pages during build
-  // This prevents Firebase auth errors during build time
-  exportPathMap: async function () {
-    return {};
-  },
 }
 
 module.exports = nextConfig
