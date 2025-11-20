cf44b894d349160074d961b8a0a9e9cde352bd5b
Eduardo Lobos <eduardoandres.lobos@gmail.com>
2025-11-20 18:40:23 +0000

    Disable pre-rendering to avoid Firebase build errors

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  output: 'standalone',
+  experimental: {
+    // Disable static page generation for pages that use Firebase
+    outputFileTracingIncludes: {
+      '/': ['./node_modules/**/*.wasm', './node_modules/**/*.node'],
+    },
+  },
+  // Don't try to prerender pages during build
+  // This prevents Firebase auth errors during build time
+  exportPathMap: async function () {
+    return {};
+  },
 }
 
 module.exports = nextConfig
